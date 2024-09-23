e93ae563867dae43d27a3e2746e2baa4a8aeb47d
ArmandoG <armandopinedag@outlook.com>
2024-09-23 11:50:03 -0700

    complete crud operations to model

--- VideoGame.Api/Program.cs
+++ VideoGame.Api/Program.cs
@@ -1,6 +1,9 @@
 
 using VideoGame.Api.Entities;
 
+//const to endpoint definition
+const string GetGameEndpointName = "GetGame";
+
 // In-memory list of games
 List<Game> games = new()
 {
@@ -43,7 +46,6 @@ app.MapGet("/games", () => games);
 //GET by {id}
 app.MapGet("/games/{id}", (int id) => 
 {
-
     Game? game = games.Find(game => game.gameId == id); //"?" nullable value
 
     if(game is null) //validate not null value
@@ -51,6 +53,51 @@ app.MapGet("/games/{id}", (int id) =>
         return Results.NotFound();
     }
     return Results.Ok(game);
+})
+.WithName(GetGameEndpointName); //specify a name for our endopoint
+
+//POST new game
+app.MapPost("/games", (Game game) => {
+    game.gameId = games.Max(game => game.gameId) + 1; //assign max id val
+    games.Add(game); //Add to list of 'games'
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.gameId}, game);
+    /*
+    1st arg -> endpoint name
+    2nd arg -> anon obj def for the needed Id
+    3rd arg -> the entire obj just created
+    */
+});
+
+//PUT existing game
+app.MapPut("/games/{givenId}", (int givenId, Game updatedGame) => {
+
+    Game? existingGame = games.Find(game => game.gameId == givenId); //"?" nullable value
+
+    if(existingGame is null) //validate null value
+    {
+        return Results.NotFound(); //TO RE-WORK
+    }
+
+    existingGame.Name = updatedGame.Name;
+    existingGame.Genre = updatedGame.Genre;
+    existingGame.Price = updatedGame.Price;
+    existingGame.ReleaseDate = updatedGame.ReleaseDate;
+    existingGame.ImageUrl = updatedGame.ImageUrl;
+    
+    return Results.NoContent(); //usual with PUT
+});
+
+//DELETE method
+app.MapDelete("/games/{givenId}", (int givenId) => {
+    Game? givenGame = games.Find(game => game.gameId == givenId); //"?" nullable value
+
+    if(givenGame is not null) //validate not null value
+    {
+        games.Remove(givenGame);
+    }
+
+    return Results.NoContent(); //return no-content anyway
 });
 
 app.Run();
