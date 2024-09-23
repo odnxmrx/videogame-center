39e413575dfc8af30684e6e75fce5942e5153479
ArmandoG <armandopinedag@outlook.com>
2024-09-23 13:08:52 -0700

    implement repository design pattern

---
+++ VideoGame.Api/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,80 @@
+using VideoGame.Api.Entities;
+using VideoGame.Api.Repositories;
+
+namespace VideoGame.Api.Endpoints;
+
+public static class GamesEndpoints //essential extension method (static)
+{
+
+    const string GetGameEndpointName = "GetGame"; //const to endpoint definition
+
+    //endopoint management method - 'MapGamesEndpoints'
+    public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes)
+    //'this' to implement this existing Method and extend it, as 'routes'
+    {
+
+        InMemGamesRepository repository = new();
+
+
+        var routeGroup = routes.MapGroup("/games") //Defining map group for routes
+                    .WithParameterValidation(); //from NuGet 'MinimalApis.Extensions'
+
+        //GET All 
+        routeGroup.MapGet("/", () => repository.GetAll());
+
+        //GET by {id}
+        routeGroup.MapGet("/{id}", (int id) => 
+        {
+            Game? game = repository.Get(id); //games.Find(game => game.gameId == id); //"?" nullable value
+
+            return game is not null ? Results.Ok(game) : Results.NotFound();
+        })
+        .WithName(GetGameEndpointName); //specify a name for our endopoint
+
+        //POST new game
+        routeGroup.MapPost("/", (Game game) => {
+            repository.Create(game);
+            return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.gameId}, game);
+            /*
+            1st arg -> endpoint name
+            2nd arg -> anon obj def for the needed Id
+            3rd arg -> the entire obj just created
+            */
+        });
+
+        //PUT existing game
+        routeGroup.MapPut("/{givenId}", (int givenId, Game updatedGame) => {
+
+            Game? existingGame = repository.Get(givenId); //games.Find(game => game.gameId == givenId); //"?" nullable value
+
+            if(existingGame is null) //validate null value
+            {
+                return Results.NotFound(); //TO RE-WORK
+            }
+
+            existingGame.Name = updatedGame.Name;
+            existingGame.Genre = updatedGame.Genre;
+            existingGame.Price = updatedGame.Price;
+            existingGame.ReleaseDate = updatedGame.ReleaseDate;
+            existingGame.ImageUrl = updatedGame.ImageUrl;
+            
+            repository.Update(existingGame); //execute updating
+
+            return Results.NoContent(); //usual with PUT
+        });
+
+        //DELETE method
+        routeGroup.MapDelete("/{givenId}", (int givenId) => {
+            Game? givenGame = repository.Get(givenId); //games.Find(game => game.gameId == givenId); //"?" nullable value
+
+            if(givenGame is not null) //validate not null value
+            {
+                repository.Delete(givenId); //games.Remove(givenGame);
+            }
+
+            return Results.NoContent(); //return no-content anyway
+        });
+
+        return routeGroup; //our RouteGroupBuilder
+    }
+}
---
+++ VideoGame.Api/Repositories/InMemGamesRepository.cs
@@ -0,0 +1,75 @@
+using System;
+using VideoGame.Api.Entities;
+
+namespace VideoGame.Api.Repositories;
+
+public class InMemGamesRepository
+{
+
+    // In-memory list of games
+    private readonly List<Game> games = new()
+    {
+        new Game()
+        {
+            gameId = 1,
+            Name = "Street Figther II",
+            Genre = "Figthing",
+            Price = 19.99M,
+            ReleaseDate = new DateTime(1991, 2, 1),
+            ImageUrl = "https://placehold.co/100"
+        },
+        new Game()
+        {
+            gameId = 2,
+            Name = "Final Fantasy XIV",
+            Genre = "Roleplaying",
+            Price = 29.99M,
+            ReleaseDate = new DateTime(2010, 9, 30),
+            ImageUrl = "https://placehold.co/100"
+        },
+        new Game()
+        {
+            gameId = 3,
+            Name = "FIFA 203",
+            Genre = "Sports",
+            Price = 59.99M,
+            ReleaseDate = new DateTime(2022, 9, 27),
+            ImageUrl = "https://placehold.co/100"
+        },
+        
+    };
+
+    //retrieve all games
+    public IEnumerable<Game> GetAll()
+    {
+        return games;
+    }
+
+    //Get single game
+    public Game? Get(int id)
+    {
+        return games.Find(game => game.gameId == id); //"?" nullable value
+    }
+
+    //Create new
+    public void Create(Game game)
+    {
+        game.gameId = games.Max(game => game.gameId) + 1; //assign max id val
+        games.Add(game); //Add to list of 'games'
+    }
+
+    //Update
+    public void Update(Game updatedGame)
+    {
+        var index = games.FindIndex(game => game.gameId == updatedGame.gameId);
+        games[index] = updatedGame;
+    }
+
+    //Delete
+    public void Delete(int id)
+    {
+        var index = games.FindIndex(game => game.gameId == id);
+        games.RemoveAt(index);
+    }
+
+}
